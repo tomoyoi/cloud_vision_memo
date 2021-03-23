d9e035ffb99e90d7ea0f00b0248ff2b866e32066
Tomoyo Ito <tmyooo73@gmail.com>
2021-03-23 17:17:42 +0900

    add task

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,8 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
+    field :delete_task, mutation: Mutations::DeleteTask
+    field :update_task, mutation: Mutations::UpdateTask
+    field :create_task, mutation: Mutations::CreateTask
     field :test_field, String, null: false,
       description: "An example field added by the generator"
     def test_field
