36f76534f89c65a9544047fd80fe91622b607103
Tomoyo Ito <tmyooo73@gmail.com>
2021-03-30 09:55:57 +0900

    add mysql

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_03_23_070218) do
 
-  create_table "tasks", force: :cascade do |t|
+  create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.boolean "completed"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_03_23_070218) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
